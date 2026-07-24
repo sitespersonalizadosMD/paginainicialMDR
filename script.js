b4826f7376f5ba0378ca760c86ffcb9f4eb46b52
sitespersonalizadosMD <matheusrodrigues3001@gmail.com>
2026-07-24 14:19:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const navbar = document.getElementById("navbar");
 
+window.addEventListener("scroll", ()=>{
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    }
+
+    else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});

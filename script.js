c30662bee731b5caeb291d48a987ce32bcda53e3
sitespersonalizadosMD <matheusrodrigues3001@gmail.com>
2026-07-24 14:56:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ window.addEventListener("scroll", ()=>{
     }
 
 });
+const faqs = document.querySelectorAll(".faq-item");
+
+faqs.forEach(faq => {
+
+    faq.querySelector(".faq-question").addEventListener("click", () => {
+
+        faq.classList.toggle("active");
+
+    });
+
+});

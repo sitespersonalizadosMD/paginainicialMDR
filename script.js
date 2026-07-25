dbee401b636f20d2abf4eac81925c30154521849
sitespersonalizadosMD <matheusrodrigues3001@gmail.com>
2026-07-25 14:15:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,32 @@ mobileMenu.querySelectorAll("a").forEach(link => {
     });
 
 });
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+
+            counter.innerText = `${Math.ceil(count + increment)}`;
+
+            setTimeout(updateCounter,20);
+
+        }else{
+
+            counter.innerText = target + "+";
+
+        }
+
+    }
+
+    updateCounter();
+
+});
